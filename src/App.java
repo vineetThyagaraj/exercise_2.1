58a4aa1991768fdc6b15b374cbc27248d20d30be
vineet-thyagaraj <vineetgowda.thyagaraj@tekeinstein.com>
2025-02-02 00:21:31 -0500

    Exercise_2.1

---
+++ src/App.java
@@ -0,0 +1,28 @@
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner celsius_Input = new Scanner(System.in);
+
+        double fahrenheit = 0;
+        double celsius = 0;
+
+        System.out.print("Enter temperature in celsius: ");
+        celsius = celsius_Input.nextDouble();
+        fahrenheit = convertToFahrenheit(celsius);
+        
+
+        System.out.println("Temperature in Fahrenheit is " + fahrenheit);
+
+    }
+
+    public static double convertToFahrenheit ( double celsius){
+         DecimalFormat df_obj = new DecimalFormat("#.#");
+        df_obj.setRoundingMode(RoundingMode.FLOOR);
+        double cel = Double.parseDouble(df_obj.format((9.0 / 5) * celsius + 32));
+        return cel;
+    }
+
+}
---
+++ src/AppTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testConvertToFahrenheit() {
+        App test = new App();
+        assertEquals( 32.0, test.convertToFahrenheit(0), 0);
+        assertEquals( 212.0, test.convertToFahrenheit(100), 0);
+        assertEquals( -40.0, test.convertToFahrenheit(-40), 0);
+        assertEquals( 98.6, test.convertToFahrenheit(37), 0);
+
+    }
+}
