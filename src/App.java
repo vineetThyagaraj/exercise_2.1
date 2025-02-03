c66c5d704bd3948ad81ffc8d993a8c352763a86d
vineet-thyagaraj <vineetgowda.thyagaraj@tekeinstein.com>
2025-02-02 21:54:21 -0500

    closed scanner

--- src/App.java
+++ src/App.java
@@ -5,17 +5,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner celsius_Input = new Scanner(System.in);
-
         double fahrenheit = 0;
         double celsius = 0;
-
         System.out.print("Enter temperature in celsius: ");
         celsius = celsius_Input.nextDouble();
         fahrenheit = convertToFahrenheit(celsius);
-        
-
         System.out.println("Temperature in Fahrenheit is " + fahrenheit);
-
+        celsius_Input.close();
     }
 
     public static double convertToFahrenheit ( double celsius){
